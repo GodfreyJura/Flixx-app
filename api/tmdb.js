1706f666efbcfae524e8ec9878ba94caec299ac2
GodfreyJura <godfreyjura382@gmail.com>
2025-08-29 10:50:06 +0300

    Configured TMDb API key using environment variables

--- api/tmdb.js
+++ api/tmdb.js
@@ -16,7 +16,7 @@ export default async function handler(req, res) {
         accept: "application/json",
       },
     });
-
+ 
     if (!response.ok) {
       return res.status(response.status).json({ error: "Failed to fetch from TMDB" });
     }
